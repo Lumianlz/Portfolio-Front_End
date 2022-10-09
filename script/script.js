4ecd4693b0166983476c5bb9e1a7867a2025506e
Lumianlz <88009335+Lumianlz@users.noreply.github.com>
2022-10-08 21:38:53 -0300

    si

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-fetch( "/main.json").then((Response)=>Response.json()).then((users)=> console.log(users))
+fetch( 'script/main.json').then((response)=>response.json()).then((users)=>
+    {
+       const user1 = users[0].nombre;
+       let yo= user1;
+    }
+    
+    );
+// script para nav
