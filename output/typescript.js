e78642cadb881ffcf7f4cb55dc903a3f4cf87571
Lumianlz <88009335+Lumianlz@users.noreply.github.com>
2022-10-08 22:53:39 -0300

    typesript

---
+++ output/typescript.js
@@ -0,0 +1,8 @@
+"use strict";
+let numeros;
+numeros = 23;
+let numeros_2 = 56;
+let strings;
+strings = "hola soy un string";
+let string_2 = "soy otro string implicito";
+console.log(string_2);
---
+++ typescript.ts
@@ -0,0 +1,7 @@
+let numeros:number;
+numeros=23;
+let numeros_2=56;
+let strings:string;
+strings="hola soy un string";
+let string_2="soy otro string implicito";
+console.log(string_2);
