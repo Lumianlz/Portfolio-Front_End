3f424b5db7f6e5d6c4ecb7e0c1f758a7fef05905
Lumianlz <88009335+Lumianlz@users.noreply.github.com>
2022-10-10 15:16:29 -0300

    10/10

--- output/typescript.js
+++ output/typescript.js
@@ -5,4 +5,16 @@ let numeros_2 = 56;
 let strings;
 strings = "hola soy un string";
 let string_2 = "soy otro string implicito";
-console.log(string_2);
+class personas {
+    constructor(nombre, apellido, nacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.nacimiento = nacimiento;
+    }
+    edad(año_actual) {
+        let edad = año_actual - this.nacimiento;
+        return edad;
+    }
+}
+let persona_1 = new personas("juan", "lopez", 1820);
+console.log(persona_1.edad(2022));
--- typescript.ts
+++ typescript.ts
@@ -4,4 +4,19 @@ let numeros_2=56;
 let strings:string;
 strings="hola soy un string";
 let string_2="soy otro string implicito";
-console.log(string_2);
+
+class personas {
+    private nombre:string;
+    private apellido:string;
+    private nacimiento:number;
+    constructor  (nombre:string, apellido:string, nacimiento:number){
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.nacimiento=nacimiento;
+    }
+public edad(año_actual:number){
+    return año_actual-this.nacimiento;
+}
+}
+let persona_1= new personas("juan", "lopez", 1820);
+console.log(persona_1.edad);
